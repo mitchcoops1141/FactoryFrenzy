8a2d8d21e00d9c154a5733907fc3983da6133e6c
mitchcoops1141 <mitchcoops1141@gmail.com>
2021-12-21 12:50:36 +1100

    grid implemented

---
+++ FactoryFrenzyUnityProj/Assets/Scenes/GridManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private GameObject cell;
+    [SerializeField] private int cellWidth = 1;
+    [SerializeField] private int cellHeight = 1;
+
+    [SerializeField] private int gridWidth;
+    [SerializeField] private int gridHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cacheCell = null;
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int z = 0; z < gridHeight; z++)
+            {
+                cacheCell = Instantiate(cell);
+                cacheCell.transform.position = new Vector3(x, 0, -z);
+            }
+        }
+
+        Camera.main.transform.position = new Vector3(gridWidth / 2, Camera.main.transform.position.y, -gridHeight / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
